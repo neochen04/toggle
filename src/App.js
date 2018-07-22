9c37b542c1703df22e3440de0bcf50eebc8c7ed4
neo <terryku01421013@gmail.com>
2018-07-22 12:59:40 +1000

    Fix - minor issue

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ function isHidden(el) {
 class App extends Component {
   handleHeaderClick = (e) => {
     const headerEl = e.currentTarget;
-    const bodyEl = headerEl.parentNode.querySelector('.toggle-body');
+    const bodyEl = headerEl.parentNode.querySelector('.toggle-body'); // querySelectAll
     const isVisible = !isHidden(bodyEl);
-    el.style.display = (isVisible) ? 'none' : 'block';
+    bodyEl.style.display = (isVisible) ? 'none' : 'block';
   };
   render() {
     return (
       <div className="App">
         <h1>"Toggle" Component</h1>
+
         <Toggle>
           <div className="toggle-header" onClick={this.handleHeaderClick}>
             General
@@ -27,6 +28,7 @@ class App extends Component {
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </div>
         </Toggle>
+
         <Toggle>
           <div className="toggle-header" onClick={this.handleHeaderClick}>
             General
